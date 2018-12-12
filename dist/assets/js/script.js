8634739da1dce38a0f8b10d33d76d7a968592f94
nereid <nas.kuleshova@gmail.com>
2018-12-12 13:58:53 +0300

    add sliders

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -133,4 +133,29 @@ $(document).ready(function () {
     $('.banner__icons').addClass('animate');
   }
 });
+
+if ($('.reviews__slider').length > 0) {
+  $('.reviews__slider').slick({
+    slidesToShow: 3,
+    nextArrow: '<button class="slider__btn slider__next"><img src="assets/images/icons/arrow.svg" alt=""></button>',
+    prevArrow: '<button class="slider__btn slider__prev"><img src="assets/images/icons/arrow.svg" alt=""></button>',
+    responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2
+      }
+    }, {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1
+      }
+    }, {
+      breakpoint: 576,
+      settings: {
+        arrows: false,
+        slidesToShow: 1
+      }
+    }]
+  });
+}
 //# sourceMappingURL=script.js.map
